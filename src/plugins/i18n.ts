fb7fe54f5a5b5f8f0d5ff947249c6551c26421b1
Roberto Mitugno <roberto.mitugno@studio.unibo.it>
2026-01-21 13:29:13 +0100

    feat: add English language support

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -1,11 +1,13 @@
 import { createI18n } from 'vue-i18n'
 import it from '../i18n/it.json'
+import en from '../i18n/en.json'
 
 export const i18n = createI18n({
   legacy: false, // Use Composition API mode
   locale: 'it',
   fallbackLocale: 'it',
   messages: {
-    it
+    it,
+    en
   }
 })
